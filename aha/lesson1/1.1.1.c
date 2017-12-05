8abae1d2edabe96dae6ac172b2a6d1ef13ab5a27
shunwuyu <shunwu2001@163.com>
2017-12-05 11:24:41 +0800

    由大到小

---
+++ aha/lesson1/1.1.1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  // 对1000以内的数排序
+  int book[1001], i, j, t, n;
+  // 初始化
+  for (i = 0; i <= 1000; i++)
+    book[i] = 0;
+  
+  scanf("%d", &n);
+  
+  for (i = 1; i <= n; i++) {
+    scanf("%d", &t);
+    book[t]++;
+  }
+
+  for (i=1000;i>=0;i--) {
+    for (j=1;j<=book[i];j++) {
+      printf("%d\n", i);
+    }
+  }
+  getchar();
+  getchar();
+  return 0;
+}
